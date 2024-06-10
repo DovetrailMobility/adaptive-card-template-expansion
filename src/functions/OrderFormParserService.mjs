eaf25331c390642797ee3d22da0c2a0fa933d38c
Olie Denyer <olied@anma.io>
2024-06-10 19:22:43 +0000

    Added orderUrl to response

--- src/functions/OrderFormParserService.mjs
+++ src/functions/OrderFormParserService.mjs
@@ -16,32 +16,37 @@ app.http("OrderFormParserService", {
 
       // parse order request
       const orderResponse = {};
-      orderResponse.orderId = String(orderRequest?.id);
-      orderResponse.status = orderRequest?.status;
-      orderResponse.dateCreated = orderRequest?.date_created;
-      orderResponse.total = orderRequest?.total;
+      orderResponse.orderId = String(orderRequest.id);
+      orderResponse.status = orderRequest.status;
+      orderResponse.dateCreated = orderRequest.date_created;
+      orderResponse.total = orderRequest.total;
+      orderResponse.orderUrl = orderRequest._links.self[0].href;
       orderResponse.billingAddress = {};
-      orderResponse.billingAddress.firstName = orderRequest?.billing.first_name;
-      orderResponse.billingAddress.lastName = orderRequest?.billing.last_name;
-      orderResponse.billingAddress.email = orderRequest?.billing.email;
-      orderResponse.billingAddress.phone = orderRequest?.billing.phone;
-      orderResponse.billingAddress.address1 = orderRequest?.billing.address_1;
-      orderResponse.billingAddress.address2 = orderRequest?.billing.address_2;
-      orderResponse.billingAddress.city = orderRequest?.billing.city;
-      orderResponse.billingAddress.state = orderRequest?.billing.state;
-      orderResponse.billingAddress.postcode = orderRequest?.billing.postcode;
-      orderResponse.billingAddress.country = orderRequest?.billing.country;
+      orderResponse.billingAddress.firstName =
+        orderRequest?.billing?.first_name;
+      orderResponse.billingAddress.lastName = orderRequest?.billing?.last_name;
+      orderResponse.billingAddress.email = orderRequest?.billing?.email;
+      orderResponse.billingAddress.phone = orderRequest?.billing?.phone;
+      orderResponse.billingAddress.address1 = orderRequest?.billing?.address_1;
+      orderResponse.billingAddress.address2 = orderRequest?.billing?.address_2;
+      orderResponse.billingAddress.city = orderRequest?.billing?.city;
+      orderResponse.billingAddress.state = orderRequest?.billing?.state;
+      orderResponse.billingAddress.postcode = orderRequest?.billing?.postcode;
+      orderResponse.billingAddress.country = orderRequest?.billing?.country;
       orderResponse.shippingAddress = {};
       orderResponse.shippingAddress.firstName =
-        orderRequest?.shipping.first_name;
-      orderResponse.shippingAddress.lastName = orderRequest?.shipping.last_name;
-      orderResponse.shippingAddress.phone = orderRequest?.shipping.phone;
-      orderResponse.shippingAddress.address1 = orderRequest?.shipping.address_1;
-      orderResponse.shippingAddress.address2 = orderRequest?.shipping.address_2;
-      orderResponse.shippingAddress.city = orderRequest?.shipping.city;
-      orderResponse.shippingAddress.state = orderRequest?.shipping.state;
-      orderResponse.shippingAddress.postcode = orderRequest?.shipping.postcode;
-      orderResponse.shippingAddress.country = orderRequest?.shipping.country;
+        orderRequest?.shipping?.first_name;
+      orderResponse.shippingAddress.lastName =
+        orderRequest?.shipping?.last_name;
+      orderResponse.shippingAddress.phone = orderRequest?.shipping?.phone;
+      orderResponse.shippingAddress.address1 =
+        orderRequest?.shipping?.address_1;
+      orderResponse.shippingAddress.address2 =
+        orderRequest?.shipping?.address_2;
+      orderResponse.shippingAddress.city = orderRequest?.shipping?.city;
+      orderResponse.shippingAddress.state = orderRequest?.shipping?.state;
+      orderResponse.shippingAddress.postcode = orderRequest?.shipping?.postcode;
+      orderResponse.shippingAddress.country = orderRequest?.shipping?.country;
       orderResponse.paymentMethod = orderRequest?.payment_method;
       orderResponse.paymentMethodTitle = orderRequest?.payment_method_title;
       orderResponse.lineItems = [];
